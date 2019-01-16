8869b5dc79a8bd21098d4bffac6cab29e1e7ea7e
Johny.li <johny.li@selmit.com>
2019-01-16 13:13:11 +0800

    update bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,24 +1,31 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
 
 // Import typefaces
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
 import profilePic from './profile.jpg'
+
 import { rhythm } from '../utils/typography'
 
-class Bio extends React.Component {
-  render() {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          marginBottom: rhythm(2.5),
-        }}
-      >
-        <img
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social } = data.site.siteMetadata
+        return (
+          <div
+            style={{
+              display: `flex`,
+              marginBottom: rhythm(2.5),
+            }}
+          >
+          <img
           src={profilePic}
-          alt={`红恩`}
+          alt={author}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -26,12 +33,28 @@ class Bio extends React.Component {
             height: rhythm(2),
           }}
         />
-        <p style={{ maxWidth: 310 }}>
-        Personal blog by <a href="https://weibo.com/0xfff">HongEn Li</a>
-        </p>
-      </div>
-    )
-  }
+            <p style={{ maxWidth: 310 }}>
+            Personal blog by <a href={`https://weibo.com/${social.weibo}`}>{author}</a>
+            </p>
+          </div>
+        )
+      }}
+    />
+  )
 }
+
+const bioQuery = graphql`
+  query BioQuery {
+    site {
+      siteMetadata {
+        author
+        social {
+          twitter
+          weibo
+        }
+      }
+    }
+  }
+`
 
 export default Bio
